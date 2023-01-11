b0f190658dd9a14ca8afe89f32607d82974d8d36
Mario <xmariosalman@gmail.com>
2023-01-11 12:12:20 +0200

    added const to sums

--- Problema 2/script2.js
+++ Problema 2/script2.js
@@ -1,10 +1,10 @@
-x = 5;
-y = 8;
+const x = 5;
+const y = 8;
 // Exercitul 1
 
-sum1 = 2 * x - 5 * y;
+const sum1 = 2 * x - 5 * y;
 console.log(sum1);
 
 // Exercitiul 2
-sum2 = 3 * x + 9 * y;
+const sum2 = 3 * x + 9 * y;
 console.log(sum2);
